669e8c85e83330c24193ffcfd3ad4efadd494e97
umar1908 <umar190801@gmail.com>
2021-02-07 03:22:58 +0300

    first commit

---
+++ task1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+// #include <functional>
+// #include <algorithm>
+// #include <vector>
+// #include <string>
+// #include <cmath>
+// #include <cstdlib>
+// #include<utility>
+// #include <exception>
+// #include <bitset>
+
+int main(){
+  system("chcp 1251"); //для корректного отображения кирилицы
+  cout<<endl;
+
+
+}
